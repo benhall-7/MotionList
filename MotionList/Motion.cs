b657c73f8d4058a08c5ae640f9057a5c64303798
BenHall-7 <benjaminjahall@gmail.com>
2019-01-24 12:57:34 -0500

    Generalize hash reading based on Size

--- MotionList/Motion.cs
+++ MotionList/Motion.cs
@@ -9,7 +9,7 @@ namespace MotionList
         public ushort Flags { get; set; }//replace this with individual flags later
         public byte Frames { get; set; }
         public bool HasAnimation { get; set; }
-        public int Unk1 { get; set; }//always 0x1c?
+        private int Size { get; set; }
         public ulong AnimationHash { get; set; }
         public int Unk2 { get; set; }
         public ulong ExpressionHash { get; set; }
@@ -19,6 +19,11 @@ namespace MotionList
         public byte XluEnd { get; set; }
         public short CancelFrame { get; set; }
 
+        public bool HasExpression { get { return Size / 8 >= 3; } }
+        public bool HasSound { get { return Size / 8 >= 2; } }
+        public bool HasEffect { get { return Size / 8 >= 1; } }
+        public bool HasXluOrCancel { get { return Size % 8 == 4; } }
+
         internal Motion(BinaryReader reader)
         {
             MotionKind = reader.ReadUInt64();
@@ -26,18 +31,24 @@ namespace MotionList
             Flags = reader.ReadUInt16();
             Frames = reader.ReadByte();
             HasAnimation = reader.ReadBoolean();
-            Unk1 = reader.ReadInt32();
+            Size = reader.ReadInt32();
             if (HasAnimation)
             {
                 AnimationHash = reader.ReadUInt64();
                 Unk2 = reader.ReadInt32();
             }
-            ExpressionHash = reader.ReadUInt64();
-            SoundHash = reader.ReadUInt64();
-            EffectHash = reader.ReadUInt64();
-            XluStart = reader.ReadByte();
-            XluEnd = reader.ReadByte();
-            CancelFrame = reader.ReadInt16();
+            if (HasExpression)
+                ExpressionHash = reader.ReadUInt64();
+            if (HasSound)
+                SoundHash = reader.ReadUInt64();
+            if (HasEffect)
+                EffectHash = reader.ReadUInt64();
+            if (HasXluOrCancel)
+            {
+                XluStart = reader.ReadByte();
+                XluEnd = reader.ReadByte();
+                CancelFrame = reader.ReadInt16();
+            }
         }
 
         internal void Write(BinaryWriter writer)
@@ -47,18 +58,24 @@ namespace MotionList
             writer.Write(Flags);
             writer.Write(Frames);
             writer.Write(HasAnimation);
-            writer.Write(Unk1);
+            writer.Write(Size);
             if (HasAnimation)
             {
                 writer.Write(AnimationHash);
                 writer.Write(Unk2);
             }
-            writer.Write(ExpressionHash);
-            writer.Write(SoundHash);
-            writer.Write(EffectHash);
-            writer.Write(XluStart);
-            writer.Write(XluEnd);
-            writer.Write(CancelFrame);
+            if (HasExpression)
+                writer.Write(ExpressionHash);
+            if (HasSound)
+                writer.Write(SoundHash);
+            if (HasEffect)
+                writer.Write(EffectHash);
+            if (HasXluOrCancel)
+            {
+                writer.Write(XluStart);
+                writer.Write(XluEnd);
+                writer.Write(CancelFrame);
+            }
         }
     }
 }
--- MotionXML/Program.cs
+++ MotionXML/Program.cs
@@ -6,40 +6,45 @@ namespace MotionXML
 {
     class Program
     {
-        static MotionFile list { get; set; }
-        static XmlDocument xml { get; set; }
+        static MotionFile MFile { get; set; }
+        static XmlDocument Xml { get; set; }
 
         static void Main(string[] args)
         {
             try
             {
-                list = new MotionFile(args[0]);
-                xml = new XmlDocument();
-                xml.AppendChild(xml.CreateXmlDeclaration("1.0", "UTF-8", null));
-                XmlNode root = NodeWithAttribute("MotionList", "ID", "0x" + list.IDHash.ToString("x10"));
-                foreach (Motion motion in list.Entries)
+                MFile = new MotionFile(args[0]);
+                Xml = new XmlDocument();
+                Xml.AppendChild(Xml.CreateXmlDeclaration("1.0", "UTF-8", null));
+                XmlNode root = NodeWithAttribute("MotionList", "ID", "0x" + MFile.IDHash.ToString("x10"));
+                foreach (Motion motion in MFile.Entries)
                 {
                     XmlNode motionNode = NodeWithAttribute("Motion", "Kind", "0x" + motion.MotionKind.ToString("x10"));
                     motionNode.AppendChild(NodeWithValue("GameHash", "0x" + motion.GameHash.ToString("x10")));
                     motionNode.AppendChild(NodeWithValue("Flags", "0x" + motion.Flags.ToString("x4")));
                     motionNode.AppendChild(NodeWithValue("TransitionFrames", motion.Frames.ToString()));
                     motionNode.AppendChild(NodeWithValue("HasAnimation", motion.HasAnimation.ToString()));
-                    motionNode.AppendChild(NodeWithValue("Unk1", motion.Unk1.ToString()));
                     if (motion.HasAnimation)
                     {
                         motionNode.AppendChild(NodeWithValue("AnimationHash", "0x" + motion.AnimationHash.ToString("x10")));
                         motionNode.AppendChild(NodeWithValue("Unk2", motion.Unk2.ToString()));
                     }
-                    motionNode.AppendChild(NodeWithValue("ExpressionHash", "0x" + motion.ExpressionHash.ToString("x10")));
-                    motionNode.AppendChild(NodeWithValue("SoundHash", "0x" + motion.SoundHash.ToString("x10")));
-                    motionNode.AppendChild(NodeWithValue("EffectHash", "0x" + motion.EffectHash.ToString("x10")));
-                    motionNode.AppendChild(NodeWithValue("XluStart", motion.XluStart.ToString()));
-                    motionNode.AppendChild(NodeWithValue("XluEnd", motion.XluEnd.ToString()));
-                    motionNode.AppendChild(NodeWithValue("CancelFrame", motion.CancelFrame.ToString()));
+                    if (motion.HasExpression)
+                        motionNode.AppendChild(NodeWithValue("ExpressionHash", "0x" + motion.ExpressionHash.ToString("x10")));
+                    if (motion.HasSound)
+                        motionNode.AppendChild(NodeWithValue("SoundHash", "0x" + motion.SoundHash.ToString("x10")));
+                    if (motion.HasEffect)
+                        motionNode.AppendChild(NodeWithValue("EffectHash", "0x" + motion.EffectHash.ToString("x10")));
+                    if (motion.HasXluOrCancel)
+                    {
+                        motionNode.AppendChild(NodeWithValue("XluStart", motion.XluStart.ToString()));
+                        motionNode.AppendChild(NodeWithValue("XluEnd", motion.XluEnd.ToString()));
+                        motionNode.AppendChild(NodeWithValue("CancelFrame", motion.CancelFrame.ToString()));
+                    }
                     root.AppendChild(motionNode);
                 }
-                xml.AppendChild(root);
-                xml.Save("output.xml");
+                Xml.AppendChild(root);
+                Xml.Save("output.xml");
             }
             catch (Exception e)
             {
@@ -50,8 +55,8 @@ namespace MotionXML
 
         static XmlNode NodeWithAttribute(string nodeName, string attrName, string attrValue)
         {
-            XmlNode node = xml.CreateElement(nodeName);
-            XmlAttribute attr = xml.CreateAttribute(attrName);
+            XmlNode node = Xml.CreateElement(nodeName);
+            XmlAttribute attr = Xml.CreateAttribute(attrName);
             attr.Value = attrValue;
             node.Attributes.Append(attr);
             return node;
@@ -59,8 +64,8 @@ namespace MotionXML
 
         static XmlNode NodeWithValue(string nodeName, string value)
         {
-            XmlNode node = xml.CreateElement(nodeName);
-            XmlNode inner = xml.CreateTextNode(value);
+            XmlNode node = Xml.CreateElement(nodeName);
+            XmlNode inner = Xml.CreateTextNode(value);
             node.AppendChild(inner);
             return node;
         }
